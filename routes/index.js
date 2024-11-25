55df90bbba95dbbdfc855483d707df87746111d9
sh0TT <shothollis@gmail.com>
2024-11-25 22:26:45 +0800

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// 定义一个简单的路由
-router.get('/', (req, res) => {
-  res.send('Welcome to the homepage!');
-});
-
-module.exports = router; // 确保导出的是router
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// 定义用户相关的路由
-router.get('/', (req, res) => {
-  res.send('User list');
-});
-
-module.exports = router; // 确保导出的是router
